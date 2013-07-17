75484e0b516c1ce6880b95e6b4102cf727f1789b
Abe Yang <abeyang@gmail.com>
2013-07-17 11:47:44 -0700

    Initial checkin
    Font Awesome not working as of this checkin.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php require_once('_header.php'); ?>
+
+<div class="container">
+
+	This is inside a normal container. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.
+	
+</div>
+
+<p>Paragraph break! It's outside of the container!</p>
+
+<div class="container">
+
+	<div class="row">
+		<div class="span4">
+			This is inside a container with grids (this section takes up 4 columns). Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.
+		</div>
+	</div>
+
+</div>
+
+<?php require_once('_footer.php'); ?>
