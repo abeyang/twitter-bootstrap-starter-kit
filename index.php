382d87e9df979a998fdda4514e57015402d40e69
Abe Yang <abeyang@gmail.com>
2013-07-30 15:19:09 -0700

    Fitted less/html for Bootstrap 3

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
 <div class="container">
 
 	<div class="row">
-		<div class="span4">
+		<div class="col-lg-4">
 			This is inside a container with grids (this section takes up 4 columns). Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.
 		</div>
-		<div class="span8">
+		<div class="col-lg-8">
 			Testing out <a href="http://fortawesome.github.io/Font-Awesome/icons/">Font Awesome</a> stuff:
 			<br /><i class="icon-dashboard icon-4x"></i> Dashboard icon! <i class="icon-camera-retro icon-4x"></i> Camera icon!
 			<i class="icon-plus-sign-alt icon-4x"></i> Cross icon!
